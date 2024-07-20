8c165e1acdba07fa18757f3786862a0400832625
Azkiya08 <azkiya.bigenmi@gmail.com>
2024-07-20 15:02:11 +0700

    Update code_arduino.ino

--- code_arduino/code_arduino.ino
+++ code_arduino/code_arduino.ino
@@ -4,6 +4,9 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include "RTClib.h"
+#include <EEPROM.h>
+#include "GravityTDS.h"
+
 
 RTC_DS3231 rtc;
 char daysOfTheWeek[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
@@ -22,12 +25,15 @@ SoftwareSerial serialtoESP(3, 4);  // RX, TX
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-const int TdsSensorPin = A1;
-#define TdsSensorPin A1
-GravityTDS gravityTds;
-float temperature = 25, tdsValue = 0;
 
-int pinPH = A0;
+#define TDS_SENSOR A0
+//#define EEPROM_SIZE 512
+//GravityTDS gravityTds;
+//float temperature = 25, tdsValue = 0;
+float tdsValue =0;
+float voltage = 0;
+
+int pinPH = A1;
 float calibration_value = 21.72; //
 int phval = 0; 
 unsigned long int avgval; 
@@ -49,7 +55,7 @@ void setup() {
   Wire.begin();
   rtc.begin();
   dht.begin();
-
+ 
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
@@ -60,15 +66,15 @@ void setup() {
      rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 //     setting manual
 //     January 21, 2014 at 3am you would call:
-     rtc.adjust(DateTime(2024, 7, 5, 16, 50, 0));
+     rtc.adjust(DateTime(2024, 7, 10, 12, 50, 0));
 
   }
    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   
-  gravityTds.setPin(TdsSensorPin);
-  gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-  gravityTds.begin();  //initialization
+//  gravityTds.setPin(TdsSensorPin);
+//  gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+//  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+//  gravityTds.begin();  //initialization
 
   pinMode(pumpa, OUTPUT);
   pinMode(pumpb, OUTPUT);
@@ -125,20 +131,27 @@ void loop() {
   ph_act = 1.0026*ph_act - 1.0077;
   Serial.println(ph_act);
 
-  gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
-  gravityTds.update();  //sample and calculate 
-  tdsValue = gravityTds.getTdsValue();  // then get the value
-  
- if (tdsValue < 139.654 ) 
- {
-    tdsValue = 0;
-
- }
- else //if (tdsValue > 139.653)
- {
-    tdsValue = 2.5904*tdsValue - 361.67;
- }
- 
+//  gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+//  gravityTds.update();  //sample and calculate 
+//  tdsValue = gravityTds.getTdsValue();  // then get the value
+//  if (tdsValue < 8.91){
+//    tdsValue = 0;
+//  } else {
+//    tdsValue = 1.1455*tdsValue - 9.2106;
+//  }
+   for (int i = 0; i < 100; i++) {
+        voltage = voltage+(analogRead(TDS_SENSOR)*3.3/1024);
+        delay(10);
+      }
+      voltage = voltage/100;
+      Serial.println(voltage);
+      float compVoltage=voltage/(1.0+0.02*(0));
+      tdsValue = (133.42*compVoltage*compVoltage*compVoltage - 255.86*compVoltage*compVoltage + 857.39*compVoltage)*0.5; //Convert voltage value to TDS value
+      if (tdsValue <97.13){
+        tdsValue = 0;
+      } else {
+        tdsValue = 2.4989*tdsValue-241.71;
+      }
   Serial.print(tdsValue,0);
   Serial.println("ppm");
 
